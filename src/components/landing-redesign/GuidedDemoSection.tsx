49735714f7e370dc10c18f73b17d6cc870737172
reza96ah-ship-it <reza96ah@gmail.com>
2026-04-30 18:00:51 +0330

    Add guided demo section

---
+++ src/components/landing-redesign/GuidedDemoSection.tsx
@@ -0,0 +1,11 @@
+import { GuidedDemo } from "../demo/GuidedDemo";
+
+export function GuidedDemoSection() {
+  return (
+    <section id="guided-demo" className="px-5 py-20">
+      <div className="mx-auto max-w-7xl">
+        <GuidedDemo />
+      </div>
+    </section>
+  );
+}
