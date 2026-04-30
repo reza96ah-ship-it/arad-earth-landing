28ac083f54f58a7d24b0133ae556921a466f760a
reza96ah-ship-it <reza96ah@gmail.com>
2026-04-30 18:18:39 +0330

    Remove cart state from redesign app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import { Header } from "./components/layout/Header";
 import { Footer } from "./components/layout/Footer";
-import { CartModal } from "./components/ecommerce/CartModal";
 import { NewHeroSection } from "./components/landing-redesign/NewHeroSection";
 import { GuidedDemoSection } from "./components/landing-redesign/GuidedDemoSection";
 import { WorkflowCardsSection } from "./components/landing-redesign/WorkflowCardsSection";
@@ -11,12 +9,9 @@ import { UseCasesSection } from "./components/landing-redesign/UseCasesSection";
 import { FinalCTASection } from "./components/landing-redesign/FinalCTASection";
 
 export default function App() {
-  const [cartOpen, setCartOpen] = useState(false);
-  const [cartCount, setCartCount] = useState(0);
-
   return (
     <main className="min-h-screen bg-[#020617] text-white">
-      <Header cartCount={cartCount} onOpenCart={() => setCartOpen(true)} />
+      <Header />
 
       <NewHeroSection />
       <GuidedDemoSection />
@@ -26,15 +21,6 @@ export default function App() {
       <UseCasesSection />
       <FinalCTASection />
       <Footer />
-
-      <CartModal
-        open={cartOpen}
-        onClose={() => setCartOpen(false)}
-        onRemove={() => {
-          setCartCount(0);
-          setCartOpen(false);
-        }}
-      />
     </main>
   );
 }
