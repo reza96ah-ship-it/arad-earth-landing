9258636603fd8866aade2f375e4d7e96f150dcc5
reza96ah-ship-it <reza96ah@gmail.com>
2026-04-30 18:02:58 +0330

    Switch app to redesign landing skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,29 @@ import { useState } from "react";
 import { Header } from "./components/layout/Header";
 import { Footer } from "./components/layout/Footer";
 import { CartModal } from "./components/ecommerce/CartModal";
-import { HeroSection } from "./components/landing/HeroSection";
-import { PositioningStrip } from "./components/landing/PositioningStrip";
-import { FeaturesSection } from "./components/landing/FeaturesSection";
-import { DemoSection } from "./components/landing/DemoSection";
-import { WorkflowSection } from "./components/landing/WorkflowSection";
-import { ModulesSection } from "./components/landing/ModulesSection";
-import { UseCasesSection } from "./components/landing/UseCasesSection";
-import { PricingSection } from "./components/landing/PricingSection";
-import { ContactSection } from "./components/landing/ContactSection";
+import { NewHeroSection } from "./components/landing-redesign/NewHeroSection";
+import { GuidedDemoSection } from "./components/landing-redesign/GuidedDemoSection";
+import { WorkflowCardsSection } from "./components/landing-redesign/WorkflowCardsSection";
+import { EngineCapabilitiesSection } from "./components/landing-redesign/EngineCapabilitiesSection";
+import { DeploymentSection } from "./components/landing-redesign/DeploymentSection";
+import { UseCasesSection } from "./components/landing-redesign/UseCasesSection";
+import { FinalCTASection } from "./components/landing-redesign/FinalCTASection";
 
 export default function App() {
   const [cartOpen, setCartOpen] = useState(false);
   const [cartCount, setCartCount] = useState(0);
 
-  function addToCart() {
-    setCartCount(1);
-    setCartOpen(true);
-  }
-
   return (
     <main className="min-h-screen bg-[#020617] text-white">
       <Header cartCount={cartCount} onOpenCart={() => setCartOpen(true)} />
 
-      <HeroSection onAddToCart={addToCart} />
-      <PositioningStrip />
-      <FeaturesSection />
-      <DemoSection />
-      <WorkflowSection />
-      <ModulesSection />
+      <NewHeroSection />
+      <GuidedDemoSection />
+      <WorkflowCardsSection />
+      <EngineCapabilitiesSection />
+      <DeploymentSection />
       <UseCasesSection />
-      <PricingSection onAddToCart={addToCart} />
-      <ContactSection />
+      <FinalCTASection />
       <Footer />
 
       <CartModal
